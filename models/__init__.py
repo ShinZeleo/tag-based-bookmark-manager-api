e5653ea1195c40dc6c6e595b86b6621599feb28e
ShinZeleo <imamqhoir28@gmail.com>
2026-04-17 21:28:17 +0800

    feat(core): register models and schemas for user, bookmark, and tag

---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from .association import bookmark_tag
+from .user import User
+from .bookmark import Bookmark
+from .tag import Tag
---
+++ schemas/__init__.py
@@ -0,0 +1,3 @@
+from .user import UserCreate, UserResponse
+from .tag import TagCreate, TagUpdate, TagResponse
+from .bookmark import BookmarkCreate, BookmarkUpdate, BookmarkResponse
